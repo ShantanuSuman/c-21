a3a965777e79b509418ff8eb6cb67438a44ab237
ShantanuSuman <103509995+ShantanuSuman@users.noreply.github.com>
2022-11-10 16:18:40 +0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,55 +1,74 @@
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
+var balloon,balloonImage1,balloonImage2;
+var database;
+var height;
 
-var ball,groundObj,leftSide,rightSide;
-var world;
-var radius = 40;
+function preload(){
+   bg =loadImage("Images/cityImage.png");
+   balloonImage1=loadAnimation("Images/HotAirBallon-01.png");
+   balloonImage2=loadAnimation("Images/HotAirBallon-01.png","Images/HotAirBallon-01.png",
+   "Images/HotAirBallon-01.png","Images/HotAirBallon-02.png","Images/HotAirBallon-02.png",
+   "Images/HotAirBallon-02.png","Images/HotAirBallon-03.png","Images/HotAirBallon-03.png","Images/HotAirBallon-03.png");
+  }
 
+//Function to set initial environment
 function setup() {
-	createCanvas(1600, 700);
-	rectMode(CENTER);
+  database=firebase.database();
+  createCanvas(1500,700);
 
-	engine = Engine.create();
-	world = engine.world;
+  balloon=createSprite(250,650,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
 
-	var ball_options={
-		isStatic:false,
-		restitution:0.3,
-		friction:0,
-		density:1.2
-	}
-
-	ball = Bodies.circle(260,100,radius/2,ball_options);
-	World.add(world,ball);
-
-	groundObj=new ground(width/2,670,width,20);
-	leftSide = new ground(1100,600,20,120);
-	rightSide = new ground(1350,600,20,120);
-
-	Engine.run(engine);
-  
+  var balloonHeight=database.ref('balloon/height');
+  balloonHeight.on("value",readHeight, showError);
+  textSize(20); 
 }
 
-
+// function to display UI
 function draw() {
-  rectMode(CENTER);
-  background(0);
+  background(bg);
 
+  if(keyDown(LEFT_ARROW)){
+    updateHeight(-10,0);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    updateHeight(10,0);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+  }
+  else if(keyDown(UP_ARROW)){
+    updateHeight(0,-10);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    balloon.scale=balloon.scale -0.005;
+  }
+  else if(keyDown(DOWN_ARROW)){
+    updateHeight(0,+10);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    balloon.scale=balloon.scale+0.005;
+  }
 
-  ellipse(ball.position.x,ball.position.y,radius,radius);
-
-  groundObj.display();
-  leftSide.display();  
-  rightSide.display();
-  
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
 }
 
-function keyPressed() {
-  	if (keyCode === UP_ARROW) {
 
-		Matter.Body.applyForce(ball,ball.position,{x:85,y:-85});
-    
-  	}
+function updateHeight(x,y){
+  database.ref('balloon/height').set({
+    'x': height.x + x ,
+    'y': height.y + y
+  })
+}
+
+function readHeight(data){
+  height = data.val();
+  console.log(height.x);
+  balloon.x = height.x;
+  balloon.y = height.y;
+}
+
+function showError(){
+  console.log("Error in writing to the database");
 }
